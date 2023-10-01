8a7361f00dcfa5e7c8e52a682cd0f093dd757cfb
Kronan1 <simonlindberg1992@gmail.com>
2023-10-01 18:14:15 +0200

    Add project files.

---
+++ RitProgrammetSimon/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RitProgrammetSimon
+{
+    internal class Board
+    {
+        public static char[,] Initialize(int sizeY, int sizeX)
+        {
+            char[,] board = new char[sizeY, sizeX];
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    board[row, col] = '!';
+                }
+            }
+            return board;
+        }
+        public static void Draw(char[,] board)
+        {
+            for (int row = 0;  row < board.GetLength(0); row++)
+            {
+                for(int col = 0; col < board.GetLength(1); col++)
+                {
+                    Console.Write(board[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ RitProgrammetSimon/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Numerics;
+
+namespace RitProgrammetSimon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cursorX = 0;
+            int cursorY = 0;
+            char savedChar = '!';
+            char toChange = '0';
+            char[,] board = Board.Initialize(20, 40);
+            
+
+            while (true)
+            {
+                Board.Draw(board);
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.W)
+                {
+
+                }
+
+                if (true)
+                {
+
+                }
+                switch (key.Key)
+                {
+                    case ConsoleKey.W:
+                        board[cursorY, cursorX] = savedChar;
+                        cursorY--;
+                        if (cursorY >= 0)
+                        {
+                            savedChar = board[cursorY, cursorX];
+                            board[cursorY, cursorX] = 'X';
+                        }
+                        else { cursorY++; }
+                        break;
+
+                    case ConsoleKey.S:
+                        board[cursorY, cursorX] = savedChar;
+                        cursorY++;
+                        if (cursorY < board.GetLength(0))
+                        {
+                            savedChar = board[cursorY, cursorX];
+                            board[cursorY, cursorX] = 'X';
+                        }
+                        else { cursorY--; }
+                        break;
+
+                    case ConsoleKey.D:
+                        board[cursorY, cursorX] = savedChar;
+                        cursorX++;
+                        if (cursorX < board.GetLength(1))
+                        {
+                            savedChar = board[cursorY, cursorX];
+                            board[cursorY, cursorX] = 'X';
+                        }
+                        else { cursorX--; }
+                        break;
+
+                    case ConsoleKey.A:
+                        board[cursorY, cursorX] = savedChar;
+                        cursorX--;
+                        if (cursorX >= 0)
+                        {
+                            savedChar = board[cursorY, cursorX];
+                            board[cursorY, cursorX] = 'X';
+                        }
+                        else { cursorX++; }
+                        break;
+
+                    case ConsoleKey.Enter:
+                        board[cursorY, cursorX] = toChange;
+                        savedChar = toChange;
+                        break;
+
+                    default:
+                        toChange = key.KeyChar;
+                        break;
+                }
+                
+                
+                Console.Clear();
+            }
+        }
+    }
+}
